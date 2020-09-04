5e43d91c29626ded1bdda38a02c7327f6fad13f4
eriber01 <59887012+eriber01@users.noreply.github.com>
2020-09-04 11:35:41 -0400

    scroll de js listo
    solo falta el resposive

--- main/main.js
+++ main/main.js
@@ -1,8 +1,50 @@
-//variables para el scrool y la home page
+//recarga la home page
 
 const btn_restar = document.querySelector('#btn-home')
-//eventos
+const btn_restarInicio = document.querySelector('#inicio')
 
 btn_restar.addEventListener('click', (eve)=>{
     location.reload()
 })
+
+btn_restarInicio.addEventListener('click',(eve)=>{
+    location.reload()
+})
+
+//controla el boton de ir hacia arriba
+
+    //analiza el scroll para hacer visible el boton
+    window.onscroll = function(){
+        console.log(document.documentElement.scrollTop)
+        if(document.documentElement.scrollTop > 100){
+            document.querySelector('#scroll-up').classList.add('display-up');
+        }else{
+            document.querySelector('#scroll-up').classList.remove('display-up');
+        }
+    }
+    
+    //boton arriba
+    Subir_o_Bajar('#scroll-up', 0)
+    
+    //boton ir a recetas
+    Subir_o_Bajar('#recetas-link', 635)
+
+    //boton ir a servisios
+    Subir_o_Bajar('#servicios-link', 2845)
+    
+    //boton ir a nosotros
+    Subir_o_Bajar('#nosotros-link', 3490)
+    
+    //boton ir a contactos
+    Subir_o_Bajar("#contactos-link", 9999999)
+    
+    
+    //logica de subir o bajar
+    function Subir_o_Bajar(referecia, distacia){
+        document.querySelector(referecia).addEventListener('click', function(){
+            window.scrollTo({
+                top: distacia,
+                behavior: 'smooth'
+            })
+        })
+    }
